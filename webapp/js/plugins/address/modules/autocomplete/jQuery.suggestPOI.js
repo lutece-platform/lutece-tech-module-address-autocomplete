3b92d0deb69150dfcbfa53bf063245691251029a
HOHL <laurent.hohl@gmail.com>
2023-02-09 15:17:13 +0100

    Redmine-24975 : Upgrade JqueryUi Autocomplete version

--- webapp/js/plugins/address/modules/autocomplete/jQuery.suggestPOI.js
+++ webapp/js/plugins/address/modules/autocomplete/jQuery.suggestPOI.js
@@ -1,11 +1,8 @@
 (function($) {
-
     // Ce script n'est utile qu'après initialisation de la configuration
-    
     var $LUTECE = window.LUTECE || (window.LUTECE = {});
     var $MAA = $LUTECE.ADDRESS_AUTOCOMPLETE || ($LUTECE.ADDRESS_AUTOCOMPLETE = {});
     var $config = $MAA.config || ($MAA.config = {});
-    
     var PROP_WS_URL = "suggestPOI.ws.url";
     var PROP_DATATYPE = "suggestPOI.ws.datatype";
     var PROP_ON_SELECT_UPDATE_DOM = "suggestPOI.param.onSelectUpdateDom";
@@ -21,22 +18,16 @@
     var PROP_PARAM_NB_RESULTS_DEFAULT = "suggestPOI.param.nbResults.default";
     var PROP_PARAM_CLIENT_ID = "suggestPOI.param.clientId";
     var PROP_PARAM_STOREADRFILTER_DEFAULT = "suggestPOI.param.storeadrfilter.default";
-    
     var EVT_NS_SPOI = ".suggestPOI";
     var EVT_SELECT = "select"
     var EVT_ERROR = "error";
-    
     var DATATYPE_JSON = "json";
     var DATATYPE_JSONP = "jsonp";
-
     var APIINPUT_SUGGESTPOI = "SUGGESTPOI";
     var APIINPUT_BAN = "BAN";
     var APIINPUT_STOREADR = "STOREADR";
-
     var STOREADR_DEFAULT_FILTER = "France";
-    
     var SEP_TYPES = ",";
-
     //Compatibility with older version where the APIINPUT and DATATYPE were not given.
     //Only SuggestPoi was available. An app could have overriden PROP_WS_URL, and now
     //we default PROP_DATATYPE and PROP_API_INPUT to the values for BAN. So attempt
@@ -45,18 +36,13 @@
         $config[PROP_DATATYPE] = DATATYPE_JSONP;
         $config[PROP_API_INPUT] = APIINPUT_SUGGESTPOI;
     }
-    
     // Pattern de création d'un nouveau plugin jQuery.
     // Rajoute une méthode suggestPOI() à tout objet jQuery sélectionné dans le DOM.
-    
     $.fn.extend({
-        
         suggestPOI: function(options, arg) {
-        
         	if (typeof(console) != "undefined") {
         		console.log(["SuggestPOI setup on:", this]);
         	}
-        
             var defaultOptions = {
                 types: $config[PROP_PARAM_TYPES_DEFAULT],
                 bantype: $config[PROP_PARAM_BANTYPE_DEFAULT],
@@ -67,18 +53,15 @@
                 storeadrfilter: $config[PROP_PARAM_STOREADRFILTER_DEFAULT],
                 nbResults: $config[PROP_PARAM_NB_RESULTS_DEFAULT]
             };
-            
             options = (
         		typeof(options) === "object" ?
 				$.extend({}, defaultOptions, options) : defaultOptions
     		);
-            
             $(this).each(function() {
                 $.suggestPOI(this, options, arg);
             });
         }
     });
-    
     /**
      * Met en place une auto-complétion de libellés de POI sur un champ texte.
      * 
@@ -94,9 +77,7 @@
      * @param arg       (opt.) Autre argument statique. 
      */
     $.suggestPOI = function(elem, options, arg) {
-    
         var jElem = $(elem);
-              
         var nbResults = options.nbResults;
         var types = options.types;
         var bantype = options.bantype;
@@ -104,15 +85,11 @@
         var banlon = options.banlon;
         var banpostcode = options.banpostcode;
         var bancitycode = options.bancitycode;
-
         var storeadrfilter = options.storeadrfilter || STOREADR_DEFAULT_FILTER;
-        
-        if ($.isArray(types)) {
+        if( $.isArray(types) ){
             types = types.join(SEP_TYPES);
         }
-            
 	    jElem.autocomplete({
-           
             minLength: $config[PROP_PARAM_QUERY_MIN_LENGTH],
             delay: $config[PROP_UI_DELAY],
 	        position:{
@@ -121,9 +98,7 @@
             // Définition de la source de données.
             // Une méthode convient pour une source distante.
 	        source: function(input, forward) {
-	        	
 	        	var jThis = $(this);
-
                 var ajax_data;
                 var ajax_url;
                 if ($config[PROP_API_INPUT] == APIINPUT_BAN) {
@@ -149,30 +124,21 @@
 	                    types: types
 	                };
                 }
-	      
 	            $.ajax({
-	              
 	                url: ajax_url,
 	                dataType: $config[PROP_DATATYPE] || DATATYPE_JSONP,
-	                
 	                data: ajax_data,
-	                
 	                success: function(data) {
-	                
 	                    if (typeof(data.error) !== "undefined") {
-	                    	
 	                    	if (typeof(console) !== "undefined") {
 				            	console.error(data.error);
 				            }
-	            
 	                    	jThis.trigger($.Event(EVT_ERROR), data.error);
 	                    }
 	                    else {
-	                    
                             // Il faut produire des objets qui comportent a minima
                             // une propriété "label" pour affichage dans la liste déroulante
                             // de suggestions.
-	                    
                             var listResults;
                             if ($config[PROP_API_INPUT] == APIINPUT_BAN) {
                                 listResults = data.features;
@@ -182,7 +148,6 @@
                                 listResults = data.result;
                             }
 	                        forward($.map(listResults, function(item) {
-	                           
                                 var item2;
                                 if ($config[PROP_API_INPUT] == APIINPUT_BAN) {
                                     item2 = {
@@ -201,8 +166,6 @@
                                 } else {
                                     item2 = item;
                                 }
-
-	                
 	                           return {	                           
 	                               label: item2.libelleTypo,
 	                               poi: item2
@@ -210,21 +173,16 @@
 	                        }));
 	                    }
 	                },
-	            
 	                error: function (xhr, ajaxOptions, thrownError) {
-	                    
 	                    if (typeof(console) !== "undefined") {
 	                        console.error([thrownError, xhr, ajaxOptions]);
 	                    }
-	                    
 	                    jThis.trigger($.Event(EVT_ERROR, thrownError));
 	                }
 	            });
 	        },
-	    
             // Un élément a été sélectionné
 	        select: function(event, ui) {
-	        
 	            if (typeof(ui.item) !== "undefined") {
                     if ($config[PROP_API_INPUT] == APIINPUT_STOREADR) {
                       var _this = this;
@@ -248,18 +206,14 @@
 	               return false;
 	           }
 	        },
-	    
 	        open: function() {
 	            $(this).removeClass("ui-corner-all").addClass("ui-corner-top");
 	        },
-	    
 	        close: function() {
 	            $(this).removeClass("ui-corner-top").addClass("ui-corner-all");
 	        }
 	    });
     };
-    
     $.suggestPOI.EVT_SELECT = EVT_SELECT + EVT_NS_SPOI; // Choix de POI effectué
     $.suggestPOI.EVT_ERROR = EVT_ERROR + EVT_NS_SPOI; // Erreur survenue
-
 })(jQuery);
